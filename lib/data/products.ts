e9d72d342272d34731b94a5ad6adbffa1e31c8a0
OKE225 <patryk.wroblewski.2006@gmail.com>
2026-09-03 22:50:09 +0200

    Implement products images from supabase bucket

--- lib/data/products.ts
+++ lib/data/products.ts
@@ -1,5 +1,8 @@
 import { createClient } from "../supabase/server";
 
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const BUCKET_NAME = "photos";
+
 export async function getAllProducts() {
   const supabase = await createClient();
 
@@ -39,9 +42,15 @@ export async function getAllProducts() {
     throw new Error(`Failed to get products from database: ${error.message}`);
   }
 
-  console.log("Products from database:", data);
+  const products =
+    data?.map((p) => ({
+      ...p,
+      imageUrl: p.image_url
+        ? `${SUPABASE_URL}/storage/v1/object/public/${BUCKET_NAME}/${p.image_url}`
+        : null,
+    })) ?? [];
 
-  return data ?? [];
+  return products;
 }
 
 export async function getProductsByCategory(categoryName: string) {
@@ -84,7 +93,63 @@ export async function getProductsByCategory(categoryName: string) {
     throw new Error(`Failed to get products from database: ${error.message}`);
   }
 
-  console.log("Products from database:", data);
+  const products =
+    data?.map((p) => ({
+      ...p,
+      imageUrl: p.image_url
+        ? `${SUPABASE_URL}/storage/v1/object/public/${BUCKET_NAME}/${p.image_url}`
+        : null,
+    })) ?? [];
 
-  return data ?? [];
+  return products;
+}
+
+export async function getProductByID(id: string) {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("products")
+    .select(
+      `
+      id,
+      category_id,
+      name,
+      price,
+      stock,
+      image_url,
+      description,
+      year,
+      screen_size_inch,
+      screen_resolution,
+      screen_type,
+      cpu,
+      ram_gb,
+      battery_size,
+      charging_wattage,
+      height_mm,
+      width_mm,
+      depth_mm,
+      weight_g,
+      category:categories!inner (
+        id,
+        name
+      )
+    `,
+    )
+    .eq("id", id);
+
+  if (error) {
+    console.error("Supabase error:", error);
+    throw new Error(`Failed to get products from database: ${error.message}`);
+  }
+
+  const products =
+    data?.map((p) => ({
+      ...p,
+      imageUrl: p.image_url
+        ? `${SUPABASE_URL}/storage/v1/object/public/${BUCKET_NAME}/${p.image_url}`
+        : null,
+    })) ?? [];
+
+  return products;
 }
